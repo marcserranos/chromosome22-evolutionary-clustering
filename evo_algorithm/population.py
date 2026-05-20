3f2ae0c2c1de3dd27f002be3640b01d26ded1732
aleixruf <aleixrufm@gmail.com>
2026-05-20 16:47:13 +0200

    Refactor evolutionary clustering package to improve code structure and enhance visualization features. Updated fitness evaluator and added new plotting options for better analysis of results.

--- evo_algorithm/population.py
+++ evo_algorithm/population.py
@@ -1,20 +1,23 @@
 import random
 from individual import Individual
 
-
 class Population:
-    """A set of candidate partitions (Individuals) evolved in one experiment."""
+    """a set of candidate partitions (Individuals) evolved in one experiment"""
 
     def __init__(self, size, num_subjects, k_groups):
-        self.members = [Individual(num_subjects, k_groups) for _ in range(size)]
+        self.members = [Individual(num_subjects, k_groups) for _ in range(size)] # creates individuals
 
     def sort_by_fitness(self):
+        '''sorts all individuals by fitness, descending order'''
         self.members.sort(key=lambda x: x.fitness, reverse=True)
 
     def best(self):
+        '''returns individual with best fitness'''
         return self.members[0]
 
     def roulette_select(self):
+        '''weighted roulette selection system that returns one individual who
+        will advance as a parent for the next generation'''
         fitnesses = [m.fitness for m in self.members]
         worst = min(fitnesses)
         weights = [f - worst + 1e-9 for f in fitnesses]
